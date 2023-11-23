b8cfe971704fdb5f2a68ca3f4eb7832b1b65f3dc
mjcancino <115103640+mjcancino@users.noreply.github.com>
2023-11-22 21:15:18 -0600

    Add files via upload

---
+++ HomeActivity.java
@@ -0,0 +1,14 @@
+package com.example.personaltutoringservice;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class HomeActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home_activity);
+
+        // Add any additional logic if needed
+    }
+}
