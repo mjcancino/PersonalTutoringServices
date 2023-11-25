db2b595f32a4db11c8d4bba7f05a7ed58c602e23
mjcancino <115103640+mjcancino@users.noreply.github.com>
2023-11-24 20:26:29 -0600

    Update HomeActivity.java

--- HomeActivity.java
+++ HomeActivity.java
@@ -2,13 +2,78 @@ package com.example.personaltutoringservice;
 
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
+import android.view.Gravity;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import com.example.personaltutoringservice.R;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class HomeActivity extends AppCompatActivity {
+    private DrawerLayout drawerLayout;
+    private ActionBarDrawerToggle actionBarDrawerToggle;
+    private ImageView imageMenu;
+    private TextView textTitle;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home_activity);
 
-        // Add any additional logic if needed
+        drawerLayout = findViewById(R.id.drawerLayout);
+        imageMenu = findViewById(R.id.imageMenu);
+        textTitle = findViewById(R.id.textTitle);
+
+        // Set up the navigation drawer toggle
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.open, R.string.close);
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+
+        // Set up the menu icon click listener
+        imageMenu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                drawerLayout.openDrawer(Gravity.LEFT);
+            }
+        });
+
+        // Set up the navigation view item click listener
+        NavigationView navigationView = findViewById(R.id.navigationView);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                // Handle navigation item clicks
+                switch (item.getItemId()) {
+                    // Handle different menu items as needed
+                    case R.id.menu_item_1:
+                        // Do something when menu item 1 is clicked
+                        break;
+                    case R.id.menu_item_2:
+                        // Do something when menu item 2 is clicked
+                        break;
+                    // Add more cases for other menu items
+                }
+
+                // Close the drawer after handling the item click
+                drawerLayout.closeDrawer(Gravity.LEFT);
+                return true;
+            }
+        });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        // Handle action bar item clicks
+        if (actionBarDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
+
+
